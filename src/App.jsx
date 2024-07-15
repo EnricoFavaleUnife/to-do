e0088a03de1704ae78a3fd784e5c4c54fb8b9e26
EnricoFavaleUnife <enrico.favale@edu.unife.it>
2024-07-15 23:09:33 +0200

    Made the add new task popup work

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
+import React, { useState } from 'react';
 import "./App.css";
 import { TaskCard } from "./Components/Task-card";
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      name: "Example of todo task",
+      className: "todo"
+    },
+    {
+      name: "Example of done task",
+      className: "done"
+    }
+  ]);
+
+  function openAddTaskPopup() {
+
+    let button = document.querySelector(".open-add-task-popup-button");
+    let popup = document.querySelector(".add-task-popup");
+  
+    if (popup.classList.contains("hidden")) {
+      popup.classList.remove("hidden");
+      button.style.transform = "rotate(45deg)";
+    } else {
+      popup.classList.add("hidden");
+      button.style.transform = "rotate(0deg)";
+    }
+  }
+  
+  function addNewTask() {
+    let popup = document.querySelector(".add-task-popup");
+    let button = document.querySelector(".open-add-task-popup-button");
+  
+    let inputTask = document.querySelector("#input-task");
+    let taskName = inputTask.value;
+  
+    inputTask.value = "";
+  
+    popup.classList.add("hidden");
+    button.style.transform = "rotate(0deg)";
+  
+    if (taskName!== "") {
+      console.log(taskName);
+      setTasks([
+       ...tasks,
+        {
+          name: taskName,
+          className: "todo"
+        }
+      ]);
+    }
+  }
+
   return (
     <>
       <div className="grid-container">
         <div className="title todo">Todo</div>
         <div className="title done">Done</div>
 
-        <TaskCard className="done" name="Create the page" />
-        <TaskCard className="done" name="Add button to open popup" />
-        <TaskCard className="todo" name="Make the popup work" />
-        <TaskCard className="todo" name="Click the circle to set the task as done" />
+        {tasks.map((task, index) => (
+          <TaskCard
+            key={index}
+            name={task.name}
+            className={task.className}
+          />
+        ))}
       </div>
 
       <div className="add-task-popup hidden">
@@ -25,26 +79,12 @@ function App() {
           placeholder="Task name"
         />
 
-        <button className="submit-task-button">Submit</button>
+        <button className="submit-task-button" onClick={addNewTask} >Submit</button>
       </div>
 
       <button className="open-add-task-popup-button" onClick={openAddTaskPopup} />
     </>
   );
-}
-
-function openAddTaskPopup() {
-
-  let button = document.querySelector(".open-add-task-popup-button");
-  let popup = document.querySelector(".add-task-popup");
-
-  if (popup.classList.contains("hidden")) {
-    popup.classList.remove("hidden");
-    button.style.transform = "rotate(45deg)";
-  } else {
-    popup.classList.add("hidden");
-    button.style.transform = "rotate(0deg)";
-  }
 }
 
 export default App;
