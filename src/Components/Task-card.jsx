8fa1e793da2d672ab4ed64cee30be0243735b945
EnricoFavaleUnife <enrico.favale@edu.unife.it>
2024-07-15 22:50:37 +0200

    Added click the circle to set the task done

--- src/Components/Task-card.jsx
+++ src/Components/Task-card.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
 import './task-card.css'; 
 
 const TaskCard = ({ name, className }) => {
+  const [taskClass, setTaskClass] = useState(className);
+
+  function setDone() {
+    setTaskClass('done');
+  }
+
   return (
-    <div className={classNames("task-card", className)}>
-      <button className='circle'></button>
+    <div className={classNames("task-card", taskClass)}>
+      <button className='circle' onClick={setDone}></button>
       <div className="card-title">{name}</div>
     </div>
   );
